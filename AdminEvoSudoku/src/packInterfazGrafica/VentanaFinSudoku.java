009ce0c116f7807c0c7a0d7ca7bb0ca30c0b659e
Alber <4lb3rduris@gmail.com>
2015-12-22 14:06:51 +0100

    VentanaFinSudoku V1

---
+++ AdminEvoSudoku/src/packInterfazGrafica/VentanaFinSudoku.java
@@ -0,0 +1,212 @@
+package packInterfazGrafica;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+
+import packModelo.Sesion;
+import packModelo.Tablero;
+
+public class VentanaFinSudoku extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JPanel panelConBorderLayout;
+	private JPanel panelConBoxLayout;
+
+	private JLabel lblTitulo;
+	
+	private JLabel lblPuntuacion;
+	
+
+	private JButton btnRetar;
+	private JButton btnCompartir;
+	private JButton btnMenu;
+	
+	
+	
+
+	private Dimension dimBtn = new Dimension(200, 30);
+	private Dimension dimVentana = new Dimension(325, 300);
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					new VentanaFinSudoku();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VentanaFinSudoku() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(dimVentana);
+		setLocationRelativeTo(null);
+
+		contentPane = new JPanel();
+		setContentPane(contentPane);
+		panelConBorderLayout = new JPanel();
+		panelConBoxLayout = new JPanel();
+
+		BorderLayout border = new BorderLayout();
+		panelConBorderLayout.setLayout(border);
+		BoxLayout box = new BoxLayout(panelConBoxLayout, BoxLayout.Y_AXIS);
+		panelConBoxLayout.setLayout(box);
+
+		contentPane.add(panelConBorderLayout);
+		panelConBorderLayout.add(panelConBoxLayout, BorderLayout.CENTER);
+
+		getTituloFinSudoku();
+		getLblPuntuacion();
+		
+		getbtnRetar();
+		getbtnCompartir();
+		getbtnMenu();
+		
+		setVisible(true);
+
+	}
+
+	private void getTituloFinSudoku() {
+	
+		lblTitulo = new JLabel("Sudoku finalizado");
+		lblTitulo.setHorizontalAlignment(0);
+		lblTitulo.setFont(new Font("Arial", Font.BOLD, 28));
+		lblTitulo.setOpaque(true);
+		lblTitulo.setForeground(Color.black);
+		
+		Border paddingBorder = BorderFactory.createEmptyBorder(10,10,10,10);
+		Border border = BorderFactory.createBevelBorder(0);
+		lblTitulo.setBorder(BorderFactory.createCompoundBorder(border,paddingBorder));
+
+		panelConBorderLayout.add(lblTitulo, BorderLayout.NORTH);
+
+	}
+
+	private void getLblPuntuacion() {
+		lblPuntuacion = new JLabel();
+		
+		String puntuacion = "0";//TODO pasar la puntuacion del Sudoku
+		lblPuntuacion.setText("Has conseguido "+puntuacion+" puntos");
+		
+		lblPuntuacion.setAlignmentX(Component.CENTER_ALIGNMENT);
+		lblPuntuacion.setHorizontalAlignment(0);
+		lblPuntuacion.setMinimumSize(dimBtn);
+		lblPuntuacion.setPreferredSize(dimBtn);
+		lblPuntuacion.setMaximumSize(new Dimension(Short.MAX_VALUE, Short.MAX_VALUE));
+		
+		
+	
+
+		panelConBoxLayout.add(Box.createRigidArea(new Dimension(0, 15)));
+		panelConBoxLayout.add(lblPuntuacion);
+	}
+	
+
+	
+
+	private void getbtnRetar() {
+		btnRetar = new JButton("Retar");
+		btnRetar.setAlignmentX(Component.CENTER_ALIGNMENT);
+		btnRetar.setMinimumSize(dimBtn);
+		btnRetar.setPreferredSize(dimBtn);
+		btnRetar.setMaximumSize(new Dimension(Short.MAX_VALUE, Short.MAX_VALUE));
+		
+		btnRetar.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(contentPane, "Has pulsado retar");
+			}
+		});
+
+		panelConBoxLayout.add(Box.createRigidArea(new Dimension(0, 15)));
+		panelConBoxLayout.add(btnRetar);
+	}
+
+	private void getbtnCompartir() {
+		btnCompartir = new JButton("Compartir");
+		btnCompartir.setAlignmentX(Component.CENTER_ALIGNMENT);
+		btnCompartir.setMinimumSize(dimBtn);
+		btnCompartir.setPreferredSize(dimBtn);
+		btnCompartir.setMaximumSize(new Dimension(Short.MAX_VALUE, Short.MAX_VALUE));
+		
+		btnCompartir.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				JOptionPane.showMessageDialog(contentPane, "Has pulsado compartir");
+			}
+			
+		});
+
+		panelConBoxLayout.add(Box.createRigidArea(new Dimension(0, 15)));
+		panelConBoxLayout.add(btnCompartir);
+	}
+
+	
+	private void getbtnMenu() {
+		btnMenu = new JButton("Menú principal");
+		btnMenu.setAlignmentX(Component.CENTER_ALIGNMENT);
+		btnMenu.setMinimumSize(dimBtn);
+		btnMenu.setPreferredSize(dimBtn);
+		btnMenu.setMaximumSize(new Dimension(Short.MAX_VALUE, Short.MAX_VALUE));
+
+		btnMenu.setEnabled(true);
+		
+		btnMenu.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					
+					//Pruebas
+					new VentanaMenuPpalUsuario();
+					/*BUENO
+					if(Sesion.obtSesion().obtNombreUsuario().equals("Admin")){
+						new VentanaMenuPpalAdministrador();
+					}
+					else{
+						new VentanaMenuPpalUsuario();
+					}
+					*/
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+				
+				dispose();
+				
+			}
+		});
+
+		panelConBoxLayout.add(Box.createRigidArea(new Dimension(0, 15)));
+		panelConBoxLayout.add(btnMenu);
+	}
+}

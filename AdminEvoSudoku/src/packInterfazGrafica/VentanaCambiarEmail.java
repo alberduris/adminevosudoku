1f62c32f70dc78d5d1fb2473404ef668308822fe
Alber <4lb3rduris@gmail.com>
2015-12-28 13:40:02 +0100

    VentanaOpciones + Ventanas cambio usuario,email y pass

---
+++ AdminEvoSudoku/src/packInterfazGrafica/VentanaCambiarEmail.java
@@ -0,0 +1,203 @@
+package packInterfazGrafica;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+
+
+public class VentanaCambiarEmail extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JPanel panelConBorderLayout;
+	private JPanel panelConBoxLayout;
+
+	private JLabel lblTitulo;
+	
+	private JLabel lbl;
+	private JTextField txt;
+	
+	private JButton btnAccion;
+	
+	private JButton btnAtras;
+
+	
+	
+
+
+	
+	private Dimension dimVentana = new Dimension(285, 265);
+	private Dimension dimAreaTexto = new Dimension(200, 25);
+	private Dimension dimBoton = new Dimension(150,30);
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					new VentanaCambiarEmail();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VentanaCambiarEmail() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(dimVentana);
+		setLocationRelativeTo(null);
+
+		contentPane = new JPanel();
+		setContentPane(contentPane);
+		panelConBorderLayout = new JPanel();
+		panelConBoxLayout = new JPanel();
+
+		BorderLayout border = new BorderLayout();
+		panelConBorderLayout.setLayout(border);
+		BoxLayout box = new BoxLayout(panelConBoxLayout, BoxLayout.Y_AXIS);
+		panelConBoxLayout.setLayout(box);
+
+		contentPane.add(panelConBorderLayout);
+		panelConBorderLayout.add(panelConBoxLayout, BorderLayout.CENTER);
+
+		getTitulo();
+		
+		getLbl();
+		getTxt();
+		
+		getBtn();
+	
+		getBtnAtras();
+		
+		setVisible(true);
+	}
+
+	private void getTitulo() {
+	
+		lblTitulo = new JLabel("Cambiar email");
+		lblTitulo.setHorizontalAlignment(0);
+		lblTitulo.setFont(new Font("Arial", Font.BOLD, 28));
+		lblTitulo.setOpaque(true);
+		lblTitulo.setForeground(Color.black);
+		
+		Border paddingBorder = BorderFactory.createEmptyBorder(10,10,10,10);
+		Border border = BorderFactory.createBevelBorder(0);
+		lblTitulo.setBorder(BorderFactory.createCompoundBorder(border,paddingBorder));
+
+		panelConBorderLayout.add(lblTitulo, BorderLayout.NORTH);
+
+	}
+
+
+	private void getLbl() {
+		lbl = new JLabel("Email");
+		lbl.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+		panelConBoxLayout.add(Box.createRigidArea(new Dimension(0, 15)));
+		panelConBoxLayout.add(lbl);
+	}
+
+	private void getTxt(){
+		txt = new JTextField();
+		txt.setFont(new Font("Serif", Font.ITALIC, 16));
+		txt.setPreferredSize(dimAreaTexto);
+	
+		
+		
+		
+		panelConBoxLayout.add(txt);
+	}
+	
+	
+	private void getBtn(){
+		btnAccion = new JButton("Cambiar");
+		btnAccion.setAlignmentX(CENTER_ALIGNMENT);
+		btnAccion.setMinimumSize(dimBoton);
+		btnAccion.setPreferredSize(dimBoton);
+		btnAccion.setMaximumSize(new Dimension(Short.MAX_VALUE, Short.MAX_VALUE));
+		
+		panelConBoxLayout.add(Box.createRigidArea(new Dimension(0,25)));
+		panelConBoxLayout.add(btnAccion);
+	}
+	
+	
+	
+	
+	private void getBtnAtras() {
+		btnAtras = new JButton("<");
+		btnAtras.setEnabled(true);
+		btnAtras.setAlignmentX(Component.LEFT_ALIGNMENT);
+		
+		
+		btnAtras.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					new VentanaOpciones();
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+				
+				dispose();
+				
+			}
+		});
+
+		panelConBoxLayout.add(Box.createRigidArea(new Dimension(0, 15)));
+		panelConBoxLayout.add(leftJustify(btnAtras));
+		
+	}
+	
+	
+	private Component leftJustify(JButton btn)  {
+	    Box  b = Box.createHorizontalBox();
+	    b.add(btn);
+	    b.add( Box.createHorizontalGlue() );
+	  
+	    return b;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ AdminEvoSudoku/src/packInterfazGrafica/VentanaOpciones.java
@@ -0,0 +1,255 @@
+//Forzar commit 2
+
+package packInterfazGrafica;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+
+import packBD.GestorBD;
+import packModelo.Sesion;
+import packModelo.Tablero;
+
+public class VentanaOpciones extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JPanel panelConBorderLayout;
+	private JPanel panelConBoxLayout;
+
+	private JLabel lblTitulo;
+	
+
+	private JButton btnCambiarNombreUsuario;
+	private JButton btnCambiarEmail;
+	private JButton btnCambiarPassword;
+	
+	private JButton btnAtras;
+	
+	
+
+	private Dimension dimBtn = new Dimension(200, 30);
+	private Dimension dimVentana = new Dimension(250, 300);
+
+	
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					new VentanaOpciones();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VentanaOpciones() {
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(dimVentana);
+		setLocationRelativeTo(null);
+
+		contentPane = new JPanel();
+		setContentPane(contentPane);
+		panelConBorderLayout = new JPanel();
+		panelConBoxLayout = new JPanel();
+
+		BorderLayout border = new BorderLayout();
+		panelConBorderLayout.setLayout(border);
+		BoxLayout box = new BoxLayout(panelConBoxLayout, BoxLayout.Y_AXIS);
+		panelConBoxLayout.setLayout(box);
+
+		contentPane.add(panelConBorderLayout);
+		panelConBorderLayout.add(panelConBoxLayout, BorderLayout.CENTER);
+
+		getTituloMenuPpalUsuario();
+		
+
+		getbtnCambiarNombreUsuario();
+		getbtnCambiarEmail();
+		getbtnCambiarPassword();
+		getBtnAtras();
+		setVisible(true);
+
+	}
+
+	private void getTituloMenuPpalUsuario() {
+	
+		lblTitulo = new JLabel("Opciones");
+		lblTitulo.setHorizontalAlignment(0);
+		lblTitulo.setFont(new Font("Arial", Font.BOLD, 28));
+		lblTitulo.setOpaque(true);
+		lblTitulo.setForeground(Color.black);
+		
+		Border paddingBorder = BorderFactory.createEmptyBorder(10,10,10,10);
+		Border border = BorderFactory.createBevelBorder(0);
+		lblTitulo.setBorder(BorderFactory.createCompoundBorder(border,paddingBorder));
+
+		panelConBorderLayout.add(lblTitulo, BorderLayout.NORTH);
+
+	}
+
+	
+	
+
+	
+
+	private void getbtnCambiarNombreUsuario() {
+		btnCambiarNombreUsuario = new JButton("Cambiar nombre usuario");
+		btnCambiarNombreUsuario.setAlignmentX(Component.CENTER_ALIGNMENT);
+		btnCambiarNombreUsuario.setMinimumSize(dimBtn);
+		btnCambiarNombreUsuario.setPreferredSize(dimBtn);
+		btnCambiarNombreUsuario.setMaximumSize(new Dimension(Short.MAX_VALUE, Short.MAX_VALUE));
+		
+		btnCambiarNombreUsuario.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					new VentanaCambiarNombreUsuario();
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+				
+				dispose();
+				
+			}
+		});
+
+		panelConBoxLayout.add(Box.createRigidArea(new Dimension(0, 15)));
+		panelConBoxLayout.add(btnCambiarNombreUsuario);
+	}
+
+	private void getbtnCambiarEmail() {
+		btnCambiarEmail = new JButton("Cambiar email");
+		btnCambiarEmail.setAlignmentX(Component.CENTER_ALIGNMENT);
+		btnCambiarEmail.setMinimumSize(dimBtn);
+		btnCambiarEmail.setPreferredSize(dimBtn);
+		btnCambiarEmail.setMaximumSize(new Dimension(Short.MAX_VALUE, Short.MAX_VALUE));
+		
+		btnCambiarEmail.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					new VentanaCambiarEmail();
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+				
+				dispose();
+				
+			}
+			
+			
+				
+		});
+
+		panelConBoxLayout.add(Box.createRigidArea(new Dimension(0, 15)));
+		panelConBoxLayout.add(btnCambiarEmail);
+	}
+
+	
+	private void getbtnCambiarPassword() {
+		btnCambiarPassword = new JButton("Cambiar contraseña");
+		btnCambiarPassword.setAlignmentX(Component.CENTER_ALIGNMENT);
+		btnCambiarPassword.setMinimumSize(dimBtn);
+		btnCambiarPassword.setPreferredSize(dimBtn);
+		btnCambiarPassword.setMaximumSize(new Dimension(Short.MAX_VALUE, Short.MAX_VALUE));
+
+		btnCambiarPassword.setEnabled(true);
+		
+		btnCambiarPassword.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					new VentanaCambiarPassword();
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+				
+				dispose();
+				
+			}
+		});
+
+		panelConBoxLayout.add(Box.createRigidArea(new Dimension(0, 15)));
+		panelConBoxLayout.add(btnCambiarPassword);
+	}
+	
+	private void getBtnAtras() {
+		btnAtras = new JButton("<");
+		btnAtras.setEnabled(true);
+		btnAtras.setAlignmentX(Component.LEFT_ALIGNMENT);
+		
+		
+		btnAtras.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					new VentanaMenuPpalUsuario();
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+				
+				dispose();
+				
+			}
+		});
+
+		panelConBoxLayout.add(Box.createRigidArea(new Dimension(0, 15)));
+		panelConBoxLayout.add(leftJustify(btnAtras));
+		
+	}
+	
+	
+	private Component leftJustify(JButton btn)  {
+	    Box  b = Box.createHorizontalBox();
+	    b.add(btn);
+	    b.add( Box.createHorizontalGlue() );
+	  
+	    return b;
+	}
+	
+	
+	
+
+}
